3bb3c3c0788092ac9b2340256c9c8492415c338b
tyler <sh0risu179@outlook.com>
2024-06-26 06:43:27 -0700

    added midi files

---
+++ source/FaustMidi.cpp
@@ -0,0 +1,5 @@
+//
+// Created by tyler on 6/26/24.
+//
+
+#include "FaustMidi.h"
---
+++ source/FaustMidi.h
@@ -0,0 +1,10 @@
+#ifndef AMATI_PP_FAUSTMIDI_H
+#define AMATI_PP_FAUSTMIDI_H
+#include "faust/midi/juce-midi.h"
+
+class FaustMidi
+{
+    FaustMidi();
+};
+
+#endif //AMATI_PP_FAUSTMIDI_H
